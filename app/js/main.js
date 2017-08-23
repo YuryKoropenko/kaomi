fa697cef52348b80a9a84d1127ae6ee6c3ad04d2
YuryKoropenko <yurykoropenko@gmail.com>
2017-08-23 16:36:29 +0300

    правки-1

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,98 @@ $(function() {
 		$(this).attr('placeholder',$(this).data('placeholder'));
 	});
 	/*убираем placeholder при клике end*/
+	/*tabs*/
+	$('.waysof__item-1 a').on('click', function() {
+		$('.waysof__left').css({
+			'display':'none'
+		});
+		$('.waysof__right').css({
+			'display':'none'
+		});
+		$('.waysof__item').removeClass('waysof__item-active');
+		
+		$('.waysof__left-1').css({
+			'display':'block'
+		});
+		$('.waysof__right-1').css({
+			'display':'block'
+		});
+		$(this).parent().addClass('waysof__item-active');
+		return false;
+	});
+	$('.waysof__item-2 a').on('click', function() {
+		$('.waysof__left').css({
+			'display':'none'
+		});
+		$('.waysof__right').css({
+			'display':'none'
+		});
+		$('.waysof__item').removeClass('waysof__item-active');
+		
+		$('.waysof__left-2').css({
+			'display':'block'
+		});
+		$('.waysof__right-2').css({
+			'display':'block'
+		});
+		$(this).parent().addClass('waysof__item-active');
+		return false;
+	});
+	$('.waysof__item-3 a').on('click', function() {
+		$('.waysof__left').css({
+			'display':'none'
+		});
+		$('.waysof__right').css({
+			'display':'none'
+		});
+		$('.waysof__item').removeClass('waysof__item-active');
+		
+		$('.waysof__left-3').css({
+			'display':'block'
+		});
+		$('.waysof__right-3').css({
+			'display':'block'
+		});
+		$(this).parent().addClass('waysof__item-active');
+		return false;
+	});
+	$('.waysof__item-4 a').on('click', function() {
+		$('.waysof__left').css({
+			'display':'none'
+		});
+		$('.waysof__right').css({
+			'display':'none'
+		});
+		$('.waysof__item').removeClass('waysof__item-active');
+		
+		$('.waysof__left-4').css({
+			'display':'block'
+		});
+		$('.waysof__right-4').css({
+			'display':'block'
+		});
+		$(this).parent().addClass('waysof__item-active');
+		return false;
+	});
+	$('.waysof__item-5 a').on('click', function() {
+		$('.waysof__left').css({
+			'display':'none'
+		});
+		$('.waysof__right').css({
+			'display':'none'
+		});
+		$('.waysof__item').removeClass('waysof__item-active');
+		
+		$('.waysof__left-5').css({
+			'display':'block'
+		});
+		$('.waysof__right-5').css({
+			'display':'block'
+		});
+		$(this).parent().addClass('waysof__item-active');
+		return false;
+	});
+	/*tabs end*/
 });
+
+
